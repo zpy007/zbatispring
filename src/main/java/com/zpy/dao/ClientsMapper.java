f19a1efba3143543b9f9e8fc963f0183b2770481
zpy007 <zpy2358@gmail.com>
2018-06-09 12:38:17 +0800

    使用了MapperScannerConfigurer

--- src/main/java/com/zpy/dao/ClientsMapper.java
+++ src/main/java/com/zpy/dao/ClientsMapper.java
@@ -1,7 +1,6 @@
 package com.zpy.dao;
 
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+
 
 import java.util.HashMap;
 
--- src/main/java/com/zpy/exe/Execute.java
+++ src/main/java/com/zpy/exe/Execute.java
@@ -1,15 +1,21 @@
 package com.zpy.exe;
 
+import com.zpy.dao.ClientsMapper;
 import com.zpy.service.ServiceImpl;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.HashMap;
 
 
 public class Execute {
 
     public void doExecute(){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-config.xml");
-        ServiceImpl impl = context.getBean("serviceImpl",ServiceImpl.class);
-        impl.doService();
+        //ServiceImpl impl = context.getBean("serviceImpl",ServiceImpl.class);
+        //impl.doService();
+        ClientsMapper mapper=(ClientsMapper) context.getBean("clientsMapper");
+        HashMap map=mapper.selectClient("150404198209020915");
+        System.out.println(map);
     }
 
     public static void main(String[] args){
